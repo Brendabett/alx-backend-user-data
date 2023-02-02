feb87ee2e1bad42cf46e9f8d9581131a20ea2aff
Brendabett <98813715+Brendabett@users.noreply.github.com>
2023-02-02 12:21:21 +0300

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,10 +11,10 @@ def hash_password(password: str) -> bytes:
     """
     Salted pass generation
     """
-    return bcrypt.hashpw(password.encode(utf-8), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ is valid?
     """
-    return bcrypt.checkpw(password.encode(utf-8), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
