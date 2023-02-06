3dcb9c8d85ceeeb91e7ce9f3e7a9a46fddb53225
Brendabett <98813715+Brendabett@users.noreply.github.com>
2023-02-06 21:11:40 +0300

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
-if auth_type == b'asic_auth':
+if auth_type == 'basic_auth':
     auth = BasicAuth()
 
 
